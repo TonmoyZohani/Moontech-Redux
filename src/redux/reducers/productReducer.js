32f108c15a6bb91f5f8e68ac807cfba223322d48
TonmoyZohani <tonmoyzohani@gmail.com>
2024-03-17 15:33:09 +0600

    remove from cart

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,20 +1,36 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes";
 
 const initialState = {
-  cart :[],
+  cart: [],
 };
 
 const productReduer = (state = initialState, action) => {
+  const selectedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
+
   switch (action.type) {
     case ADD_TO_CART:
+      if (selectedProduct) {
+        return state;
+      }
+
       return {
         ...state,
-        cart :[...state.cart,action.payload]
+        cart: [
+          ...state.cart,
+          {
+            ...action.payload,
+            quantity: action.payload.quantity ? action.payload.quantity + 1 : 1,
+          },
+        ],
       };
     case REMOVE_FROM_CART:
       return {
         ...state,
-        cart:state.cart.filter((product)=>product._id!==action.payload._id)
+        cart: state.cart.filter(
+          (product) => product._id !== action.payload._id
+        ),
       };
     default:
       return state;
