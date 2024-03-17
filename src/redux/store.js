abb09a33c40c2b6e6919838a41b3e4d63a139a76
TonmoyZohani <tonmoyzohani@gmail.com>
2024-03-17 14:28:02 +0600

    add to the cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 import productReduer from "./reducers/productReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 
-const store = createStore(productReduer);
+const store = createStore(productReduer,composeWithDevTools());
 
 export default store;
